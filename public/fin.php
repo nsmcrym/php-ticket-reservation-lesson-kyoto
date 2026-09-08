0ae994381698d69db6c17b7d2db3cb15edb2bd7e
RyumaNishimichi <ktc23a32e0006@edu.kyoto-tech.ac.jp>
2026-09-08 15:52:43 +0900

    DBハンドルを共通化

--- public/fin.php
+++ public/fin.php
@@ -57,24 +57,7 @@ $token = bin2hex(random_bytes(32));
 /* DBへの登録 */
 // DBハンドルの取得
 $config = require __DIR__ . '/../config.php';
-$db_config = $config['db'];
-$dsn = "mysql:dbname={$db_config['database']};host={$db_config['host']};port={$db_config['port']};charset={$db_config['charset']}";
-
-$opt = [
-    // セキュリティ上必須
-    PDO::ATTR_EMULATE_PREPARES => false,  // エミュレート無効
-    Pdo\Mysql::ATTR_MULTI_STATEMENTS => false,  // 複文無効
-    // お好みで
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // データ取得モード
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // エラーが発生した場合、PDOException をスロー
-];
-try {
-    $dbh = new \PDO($dsn, $db_config['user'], $db_config['pass'], $opt);
-} catch (\PDOException $e) {
-    // XXX 暫定: 本来はlogに出力する & エラーページを出力する
-    echo $e->getMessage();
-    exit;
-}
+$dbh = getDbh();
 // var_dump($dbh);
 // exit;
 
